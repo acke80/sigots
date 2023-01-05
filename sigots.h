b0fd56b7e19ae85721e976775aed7dd0f95ac3b2
Christoffer Akrin <34869094+acke80@users.noreply.github.com>
2023-01-05 20:46:04 +0100

    Update sigots.h

--- sigots.h
+++ sigots.h
@@ -2,10 +2,13 @@
 
 #include <functional>
 #include <memory>
-#include <vector>
+#include <set>
+#include <unordered_map>
 #include <type_traits>
 
-#include <iostream>
+#ifndef slots
+#define slots
+#endif
 
 namespace sigots {
 
@@ -30,14 +33,14 @@ private:
     public:
         Slot(const slot_t slot) : m_slot(slot) {}
 
-        virtual void call(Args... args) {
+        virtual void callback(Args... args) {
             m_slot(args...);
         };
 
         bool operator==(Slot& slot) {
             return isEqual(slot);
         }
-    };
+    }; // class Slot
 
     template <typename T>
     class ObjectSlot : public Slot {
@@ -46,28 +49,45 @@ private:
         T* m_object;
         void(T::* m_method)(Args...);
 
+        std::shared_ptr<T> m_objectTracker{nullptr};
+
         bool isEqual(Slot& slot) override {
-            bool b = m_object == static_cast<const ObjectSlot&>(slot).m_object;
-            return b;
+            const ObjectSlot& os = static_cast<const ObjectSlot&>(slot);
+            return m_object == os.m_object && m_method == os.m_method;
         }
 
     public:
         ObjectSlot(T* object, void(T::* method)(Args...)) :
             Slot(nullptr), m_object(object), m_method(method) {}
 
-        void call(Args... args) override {
+        ObjectSlot(std::shared_ptr<T> objectTracker, void(T::* method)(Args...)) :
+            Slot(nullptr), m_object(objectTracker.get()), m_method(method), m_objectTracker(objectTracker) {}
+
+        void callback(Args... args) override {
             (m_object->*m_method)(args...);
         };
-    };
 
-    std::vector<std::unique_ptr<Slot>> m_slots;
+        const T* getObject() {
+            return m_object;
+        }
+
+    }; // class ObjectSlot
+
+    std::set<std::unique_ptr<Slot>> m_slots;
+
+    void removeSlot(Slot& slot) {
+        auto it = std::find_if(m_slots.begin(), m_slots.end(),
+            [&](const std::unique_ptr<Slot>& s) { return *s == slot; });
+
+        if (it != m_slots.end()) m_slots.erase(it);
+    }
 
 public:
     /**
     * Connects free function as slot to signal. 
     */
     void inline connect(slot_t slot) {
-        m_slots.push_back(std::make_unique<Slot>(slot));
+        m_slots.insert(std::make_unique<Slot>(slot));
     }
 
     /**
@@ -79,28 +99,46 @@ public:
     template <typename T>
     void inline connect(T* object, void(T::*slot)(Args...)) {
         static_assert(std::is_class<T>::value);
-        m_slots.push_back(std::make_unique<ObjectSlot<T>>(object, slot));
+
+        m_slots.insert(std::make_unique<ObjectSlot<T>>(object, slot));
     }
 
     template <typename T>
-    void inline disconnect(T* object, void(T::* slot)(Args...)) {
+    void inline connect(std::shared_ptr<T> object, void(T::*slot)(Args...)) {
         static_assert(std::is_class<T>::value);
-        ObjectSlot os{ object, slot };
-        auto it = std::find_if(m_slots.begin(), m_slots.end(), [&](std::unique_ptr<Slot> const& s) {
-            return *s == os;
-        });
+        
+        m_slots.insert(std::make_unique<ObjectSlot<T>>(object, slot));
+    }
 
-        if (it != m_slots.end()) { 
-            m_slots.erase(it);
-        }
+    /**
+    * Disconnects free function slot.
+    */
+    void inline disconnect(slot_t slot) {
+        removeSlot({ slot });
+    }
+
+    /**
+    * Disconnects member function slot.
+    * @tparam T     The class that owns the member function.
+    * @param object The object instance of type T.
+    * @param slot   The member function of object to disconnect.
+    */
+    template <typename T>
+    void inline disconnect(T* object, void(T::*slot)(Args...)) {
+        static_assert(std::is_class<T>::value);
+        ObjectSlot<T> os{ object, slot };
+        removeSlot(os);
+    }
+
+    /**
+    * Disconnects all slots.
+    */
+    void inline clear() {
+        m_slots.clear();
     }
 
     void inline emit(Args... args) const {
-        std::cout << "emits: " << m_slots.size() << std::endl;
-        for (auto& s : m_slots) {
-            std::cout << "slot: " << std::endl;
-            s->call(args...);
-        }
+        for (auto& s : m_slots) s->callback(args...);
     }
 
     void operator()(Args... args) const {
